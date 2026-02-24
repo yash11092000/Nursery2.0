c355a24a34b3391abdce475104c7dcae2cba2440
Admin <yash.bhosale1109@gmail.com>
2026-02-24 23:28:47 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-
-window.addEventListener("scroll", () => {
-  document.querySelector(".custom-navbar")
-    .classList.toggle("scrolled", window.scrollY > 60);
-});
+// window.addEventListener("scroll", () => {
+//   document.querySelector(".custom-navbar").classList.toggle("scrolled", window.scrollY > 60);
+// });
 document.addEventListener("DOMContentLoaded", function () {
+
+
   new Splide("#plantSplide", {
-    type      : "loop",
-    autoplay  : true,
-    interval  : 3000,
-    speed     : 800,
-    arrows    : true,
+    type: "loop",
+    autoplay: true,
+    interval: 3000,
+    speed: 800,
+    arrows: true,
     pagination: true,
     pauseOnHover: false,
   }).mount();
 });
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
-  gsap.to("#LetterSection h1", {
-    xPercent: -50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: "#LetterSection",
-      start: "top 70%",
-      end: "bottom top",
-      scrub: 2
-      // markers: true
-    }
-  });
+gsap.to("#LetterSection h1", {
+  xPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#LetterSection",
+    start: "top 70%",
+    end: "bottom top",
+    scrub: 2
+    // markers: true
+  }
+});
 
 gsap.from(".img-left", {
   y: 120,
@@ -57,6 +57,31 @@ gsap.from(".img-right", {
   }
 });
 
+let t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#PlantInfoSection",
+    //markers: true,
+    start: "10% 50%",
+    end: "50% 50%",
+    scrub: 4
+  }
+})
+
+
+t1.to("#PlantInfoSection .LeftText", {
+  x: -400,
+  duration: 5,
+  delay: 1,
+}, "Text")
+
+t1.to("#PlantInfoSection .RightText", {
+  x: 400,
+  duration: 5,
+  delay: 1,
+}, "Text")
+
+
+
 
 const marquee = document.querySelector(".marquee");
 
@@ -66,16 +91,6 @@ gsap.to(marquee, {
   duration: 20,
   ease: "linear"
 });
-
-
-// Infinite marquee
-gsap.to(marquee, {
-  xPercent: -50,
-  repeat: -1,
-  duration: 25,
-  ease: "linear"
-});
-
 
 gsap.from(".about-text", {
   y: 60,
@@ -101,82 +116,119 @@ gsap.from(".about-image img", {
 
 
 document.querySelectorAll(".tool-icon").forEach(icon => {
-    icon.addEventListener("mouseenter", () => {
-      gsap.to(icon, {
-        y: -12,
-        rotate: -6,
-        scale: 1.12,
-        duration: 0.4,
-        ease: "power3.out"
-      });
-    });
-
-    icon.addEventListener("mouseleave", () => {
-      gsap.to(icon, {
-        y: 0,
-        rotate: 0,
-        scale: 1,
-        duration: 0.4,
-        ease: "power3.out"
-      });
+  icon.addEventListener("mouseenter", () => {
+    gsap.to(icon, {
+      y: -12,
+      rotate: -6,
+      scale: 1.12,
+      duration: 0.4,
+      ease: "power3.out"
     });
   });
+
+  icon.addEventListener("mouseleave", () => {
+    gsap.to(icon, {
+      y: 0,
+      rotate: 0,
+      scale: 1,
+      duration: 0.4,
+      ease: "power3.out"
+    });
+  });
+});
 
 gsap.from(".contact-box", {
-    x: -50,
-    opacity: 0,
-    duration: 1,
-    scrollTrigger: {
-      trigger: ".contact-section",
-      start: "top 75%"
-    }
-  });
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".contact-section",
+    start: "top 75%"
+  }
+});
 
-  gsap.from(".map-box", {
-    x: 50,
-    opacity: 0,
-    duration: 1,
-    scrollTrigger: {
-      trigger: ".contact-section",
-      start: "top 75%"
-    }
-  });
+gsap.from(".map-box", {
+  x: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".contact-section",
+    start: "top 75%"
+  }
+});
 
 
 document.getElementById("ContactForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // 🔴 THIS IS REQUIRED
+  e.preventDefault(); 
 
   const formData = new FormData(this);
 
   fetch("https://script.google.com/macros/s/AKfycbyWYwH3THlTwbnCsmJfwPiuMsm22mH21zi6lnT0JIO1Iu8vmafPRj5FRS_JstH0Bz4x/exec", {
-    method: "POST",
-    body: formData
-  })
-  .then(() => {
-    alert("Saved successfully");
-    this.reset();
-  })
-  .catch(err => console.error(err));
+      method: "POST",
+      body: formData
+    })
+    .then(() => {
+      alert("Saved successfully");
+      this.reset();
+    })
+    .catch(err => console.error(err));
 });
 
-// const treeTimeline = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: "#LeftCocoTree1", // Use first tree as trigger
-//     start: "top 20%",
-//     end: "top 70%",
-//     scrub: 4,
-//     markers:true
-//   }
-// });
 
-// // Animate trees with a slight delay between them
-// treeTimeline
-//   .fromTo("#LeftCocoTree1", 
-//     { y: 100, opacity: 0 },
-//     { y: 0, opacity: 1, duration: 0.8, ease: "power2.out" }
-//   )
-//   .fromTo("#LeftCocoTree2", 
-//     { y: 100, opacity: 0 },
-//     { y: 0, opacity: 1, duration: 0.8, ease: "power2.out" },
-//     "-=0.3" // Start second tree 0.3s before first finishes
-//   );
+let split1 = SplitText.create(".AnimatedText1", {
+  type: "words"
+})
+
+let split2 = SplitText.create(".AnimatedText2", {
+  type: "words"
+})
+
+
+
+gsap.from(split1.words, {
+   y: 100,
+    autoAlpha: 0,
+    stagger: {
+      amount: 0.5,
+      from: "random"
+    },
+    duration:3,
+    scrollTrigger:{
+      trigger:".plantSection",
+      start:"10% 80%",
+      end:"20% 90%",
+      scrub:3
+    }
+})
+
+gsap.from(split2.words, {
+   y: 100,
+    autoAlpha: 0,
+    stagger: {
+      amount: 0.5,
+      from: "random"
+    },
+    duration:3,
+    scrollTrigger:{
+    //markers:true,
+    trigger:"#AboutUsSection",
+    start:"10% 80%",
+    end:"20% 90%",
+    scrub:3
+  }
+})
+
+
+let panels = gsap.utils.toArray(".panel");
+
+gsap.to(panels, {
+  xPercent: -100 * (panels.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal-section",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (panels.length - 1),
+    end: () => "+=" + document.querySelector(".wrapper").offsetWidth
+  }
+});
